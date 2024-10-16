0c4db8e790cd7d9d40376cac2b7adac15c634b08
ly <3219747044@qq.com>
2024-10-16 22:22:43 +0800

    first commit

---
+++ src/main/java/Cal.java
@@ -0,0 +1,5 @@
+public class Cal {
+    public int Add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/TestCase.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestCase {
+    @Test
+    public void testRight() {
+        Cal cal = new Cal();
+        assertEquals(cal.Add(1, 2), 3);
+        System.out.println("success");
+    }
+
+
+
+}
