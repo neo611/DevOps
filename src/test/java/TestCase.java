e21177bf692f60192a5e56e4a5e8ff58d25b3953
try to be better <50024064+keepworking-l@users.noreply.github.com>
2024-10-17 21:22:47 +0800

    Update TestCase.java (#25)

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -8,7 +8,7 @@ public class TestCase {
     public void testRight() {
         Cal cal = new Cal();
         assertEquals(cal.Add(1, 2), 3);
-        System.out.println("pr12");
+        System.out.println("pr123");
     }
 
 
